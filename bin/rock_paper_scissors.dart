c998b588f6a6141eebad7d3d80430d747fccea3e
Alnur17 <mr.nur30@gmail.com>
2024-09-11 23:59:28 +0600

    Update rock_paper_scissors.dart
    player moves has been done

--- bin/rock_paper_scissors.dart
+++ bin/rock_paper_scissors.dart
@@ -1,7 +1,25 @@
 import 'dart:io';
 
+enum Move { rock, paper, scissors }
+
 void main() {
-  print('Rock, Paper, Scissors? (r/p/s)');
-  final input = stdin.readLineSync();
-  print(input);
+  while (true) {
+    print('Rock, Paper, Scissors? (r/p/s)'); // also work for stdout.write();
+    final input = stdin.readLineSync();
+    if (input == 'r' || input == 'p' || input == 's') {
+      var playerMove;
+      if (input == 'r') {
+        playerMove = Move.rock;
+      } else if (input == 'p') {
+        playerMove = Move.paper;
+      } else {
+        playerMove = Move.scissors;
+      }
+      print('Selected: $input');
+    } else if (input == 'q') {
+      break;
+    } else {
+      print('Invalid input');
+    }
+  }
 }
