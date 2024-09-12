499305c5471321753012a65d30ed34f7cb669703
Alnur17 <mr.nur30@gmail.com>
2024-09-12 22:45:41 +0600

    Update rock_paper_scissors.dart
    all work has been done

--- bin/rock_paper_scissors.dart
+++ bin/rock_paper_scissors.dart
@@ -1,13 +1,15 @@
 import 'dart:io';
+import 'dart:math';
 
 enum Move { rock, paper, scissors }
 
 void main() {
+  final randomNumGenarator = Random();
   while (true) {
     print('Rock, Paper, Scissors? (r/p/s)'); // also work for stdout.write();
     final input = stdin.readLineSync();
     if (input == 'r' || input == 'p' || input == 's') {
-      var playerMove;
+      Move playerMove; // also use var or enum
       if (input == 'r') {
         playerMove = Move.rock;
       } else if (input == 'p') {
@@ -15,7 +17,19 @@ void main() {
       } else {
         playerMove = Move.scissors;
       }
-      print('Selected: $input');
+      final random = randomNumGenarator.nextInt(3);
+      final aiMove = Move.values[random];
+      print('You played: $playerMove');
+      print('AI played $aiMove');
+      if (playerMove == aiMove) {
+        print('It\'s a draw.');
+      } else if (playerMove == Move.rock && aiMove == Move.scissors ||
+          playerMove == Move.paper && aiMove == Move.rock ||
+          playerMove == Move.scissors && aiMove == Move.paper) {
+        print('You win');
+      } else {
+        print('You lose');
+      }
     } else if (input == 'q') {
       break;
     } else {
